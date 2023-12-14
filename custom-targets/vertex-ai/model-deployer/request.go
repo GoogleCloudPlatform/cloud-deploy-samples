f912143d9c2ebd8bd5442b2c61b20e728255d821
Angel Montero <angelmontero@google.com>
2023-12-14 09:26:30 -0500

    Add vertex ai deployer (#15)
    * make some fixes to use new clouddeploy module
    
    * use new clouddeploy gcs methods to download manifest configuration
    
    * fix clouddeploy.yaml to use template
    
    * addalias fixes
    
    * fix gcs download paths
    
    * Supported deploy parameters are stored using customTarget/ prefix
    
    * fix configPath description
    
    * fix README,quickstart, add copyright to clouddeploy.yaml
    
    * Add copyright to more config files
    
    * address comments in request.go
    
    * address most deploy comments
    
    * address render, deploy comments. Split vertex-ai methods to its own file to make render/deploy files easier to read. Moving polling functions to its own functions as well
    
    * Add copy repo step as first step
    
    * typos and grammar
    
    * typo fixe in quickstart
    
    * expands comments for render.go
    
    * add comments on vertex ai
    
    * Create presubmit.yaml (#17)
    
    * Add sample implementation of a GitOps custom target (#14)
    
    * Introduce sample Terraform custom target implementation and quickstart
    
    * Fix terraform deployer module name, params comment, and build and register script perm
    
    * Add disclaimer in terraform readme indicating that the sample is not an officially supported Google product
    
    * Remove cloudbuild.yaml and build and register script temporarily
    
    * Update terraform sample with changes required to include the images commit sha in metadata
    
    * Fix module dependency version in terraform deployer
    
    * Fix the COMMIT_SHA.
    
    (And simplify it a little.)
    
    * Update the appearance of the build_and_register output
    
    The primary log lines are now green and start with ">> ".
    
    * Add clone repo instructions to terraform quickstart and readme in the custom target directory
    
    * Doc updates for Terraform sample and using const for sample commit sha metadata key
    
    * Add gitops custom target sample
    
    * Git Deployer sample: set username and email in git config after cloning the repo
    
    * Git deployer: set <> as the email in the git config if not provided
    
    * GitOps sample: Update README and QUICKSTART and added customTarget/gitEnablePullRequestMerge deploy parameter
    
    * Add docker build for each sample in presubmit.yaml
    
    ---------
    
    Co-authored-by: Michael Plump <plumpy@google.com>
    
    * build the model deployer in the presubmit
    
    * make changes to quickstart and readme to include information about add-alias feature in cloud deploy hook
    
    * fix replace variables script
    
    * more fixes
    
    * include add alias feature in vertex ai quick start
    
    * removing spacing between err declaration and handling
    
    * address comments
    
    * configPath points to file rather than directory
    
    * address comments
    
    ---------
    
    Co-authored-by: edwardthiele <4979401+edwardthiele@users.noreply.github.com>
    Co-authored-by: Matt Santa <mattsanta@google.com>
    Co-authored-by: Michael Plump <plumpy@google.com>

---
+++ custom-targets/vertex-ai/model-deployer/request.go
@@ -0,0 +1,173 @@
+// Copyright 2023 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/GoogleCloudPlatform/cloud-deploy-samples/custom-targets/util/clouddeploy"
+	"google.golang.org/api/aiplatform/v1"
+	"os"
+	"strconv"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+// Environment variable keys specific to the vertex ai deployer. These are provided via
+// deploy parameters in Cloud Deploy.
+const (
+	minReplicaCountEnvKey = "CLOUD_DEPLOY_customTarget_vertexAIMinReplicaCount"
+	modelEnvKey           = "CLOUD_DEPLOY_customTarget_vertexAIModel"
+	endpointEnvKey        = "CLOUD_DEPLOY_customTarget_vertexAIEndpoint"
+	aliasEnvKey           = "CLOUD_DEPLOY_customTarget_vertexAIAliases"
+	configPathKey         = "CLOUD_DEPLOY_customTarget_vertexAIConfigurationPath"
+)
+
+// deploy parameters that the custom target requires to be present and provided during render and deploy operations.
+const (
+	modelDPKey    = "customTarget/vertexAIModel"
+	endpointDPKey = "customTarget/vertexAIEndpoint"
+	aliasDPKey    = "customTarget/vertexAIAliases"
+)
+
+var addAliasesMode bool
+
+// requestHandler interface provides methods for handling the Cloud Deploy params.
+type requestHandler interface {
+	// Process processes the Cloud Deploy params.
+	process(ctx context.Context) error
+}
+
+// createRequestHandler creates a requestHandler for the provided Cloud Deploy request.
+func createRequestHandler(cloudDeployRequest interface{}, params *params, gcsClient *storage.Client, service *aiplatform.Service) (requestHandler, error) {
+
+	switch r := cloudDeployRequest.(type) {
+	case *clouddeploy.RenderRequest:
+		return &renderer{
+			req:               r,
+			params:            params,
+			gcsClient:         gcsClient,
+			aiPlatformService: service,
+		}, nil
+
+	case *clouddeploy.DeployRequest:
+		return &deployer{
+			req:               r,
+			params:            params,
+			gcsClient:         gcsClient,
+			aiPlatformService: service,
+		}, nil
+
+	default:
+		return nil, fmt.Errorf("received unsupported cloud deploy request type: %q", os.Getenv(clouddeploy.RequestTypeEnvKey))
+	}
+}
+
+// params contains the deploy parameter values passed into the execution environment.
+type params struct {
+	// The minimum replica count for the deployed model obtained via a deploy parameter
+	minReplicaCount int64
+
+	// The model to be deployed. May or may not contain a tag or version number.
+	// format is "projects/{project}/locations/{location}/models/{modelId}[@versionId|alias].
+	model string
+
+	// The endpoint where the model will be deployed
+	// format is "projects/{project}/locations/{location}/endpoints/{endpointId}.
+	endpoint string
+
+	// directory path where the renderer should look for target-specific configuration
+	// for this deployment, if not provided the renderer will check for a deployModel.yaml
+	// fie in the root working directory.
+	configPath string
+}
+
+// determineParams returns the supported params provided in the execution environment via environment variables.
+func determineParams() (*params, error) {
+
+	replicaCount, err := strconv.Atoi(os.Getenv(minReplicaCountEnvKey))
+	if err != nil {
+		replicaCount = 0
+	}
+
+	model, found := os.LookupEnv(modelEnvKey)
+	if !found {
+		fmt.Printf("Required environment variable %s not found. This variable is derived from deploy parameter: %s, please verify that a valid Vertex AI model resource name was provided through this deploy parameter.\n", modelEnvKey, modelDPKey)
+		return nil, fmt.Errorf("required environment variable %s not found", modelEnvKey)
+	}
+	if model == "" {
+		fmt.Printf("environment variable %s is empty. This variable is derived from deploy parameter: %s, please verify that a valid Vertex AI model resource name was provided through this deploy parameter.\n", modelEnvKey, modelDPKey)
+		return nil, fmt.Errorf("environment variable %s contains empty string", modelEnvKey)
+	}
+
+	endpoint, found := os.LookupEnv(endpointEnvKey)
+	if !found {
+		fmt.Printf("Required environment variable %s not found. This variable is derived from deploy parameter: %s, please verify that a valid Vertex AI model resource name was provided through this deploy parameter.\n", endpointEnvKey, endpointDPKey)
+		return nil, fmt.Errorf("required environment variable %s not found", modelEnvKey)
+	}
+	if model == "" {
+		fmt.Printf("environment variable %s is empty. This variable is derived from deploy parameter: %s, please verify that a valid Vertex AI model resource name was provided through this deploy parameter.\n", endpointEnvKey, endpointDPKey)
+		return nil, fmt.Errorf("environment variable %s contains empty string", modelEnvKey)
+	}
+
+	return &params{
+		model:           model,
+		endpoint:        endpoint,
+		minReplicaCount: int64(replicaCount),
+		configPath:      os.Getenv(configPathKey),
+	}, nil
+}
+
+// addAliasesRequest contains information needed to assign aliases to a model during a post deploy hook
+type addAliasesRequest struct {
+	// new aliases to apply to the model
+	aliases []string
+
+	// Cloud Deploy project
+	project string
+	// Cloud Deploy location.
+	location string
+	// Cloud Deploy target
+	target string
+	// Cloud Deploy delivery pipeline.
+	pipeline string
+	// Cloud Deploy release.
+	release string
+	// phase
+	phase string
+}
+
+// newAliasHandler returns a handler for processing alias assignment requests.
+func newAliasHandler(gcsClient *storage.Client) (requestHandler, error) {
+
+	aliasParameter := os.Getenv(aliasEnvKey)
+	if len(aliasParameter) == 0 {
+		return nil, fmt.Errorf("when 'add aliases mode' is enabled', at least one alias needs to be passed to the custom action through %s deploy parameter", aliasDPKey)
+	}
+
+	aliases := strings.Split(aliasParameter, ",")
+
+	request := &addAliasesRequest{
+		project:  os.Getenv(clouddeploy.ProjectEnvKey),
+		location: os.Getenv(clouddeploy.LocationEnvKey),
+		pipeline: os.Getenv(clouddeploy.PipelineEnvKey),
+		release:  os.Getenv(clouddeploy.ReleaseEnvKey),
+		target:   os.Getenv(clouddeploy.TargetEnvKey),
+		phase:    os.Getenv(clouddeploy.PhaseEnvKey),
+		aliases:  aliases,
+	}
+	return &aliasAssigner{gcsClient: gcsClient, request: request}, nil
+}
