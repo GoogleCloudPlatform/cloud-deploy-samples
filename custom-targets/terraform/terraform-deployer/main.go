321d11d9cf370cbf7dd19e9400334f6195175dd3
Cloud Deploy Team <cloud-deploy-samples-bot@google.com>
2025-08-01 09:29:08 -0700

    Fix: Added a comment to main.go in the Terraform package to describe what the package does
    PiperOrigin-RevId: 789790397

--- custom-targets/terraform/terraform-deployer/main.go
+++ custom-targets/terraform/terraform-deployer/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package main implements a Cloud Deploy Custom Target for deploying
+// Google Cloud infrastructure resources deployments using Terraform.
 package main
 
 import (
